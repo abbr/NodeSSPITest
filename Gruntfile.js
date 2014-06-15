5eba3d999bb7ee58853d920286587678ad63adef
Fred Wen <fred.wen@gov.bc.ca>
2014-06-14 17:23:31 -0700

    Initial version
    Signed-off-by: Fred Wen <fred.wen@gov.bc.ca>

---
+++ Gruntfile.js
@@ -0,0 +1,68 @@
+// Generated on 2013-12-04 using generator-angular-fullstack 1.0.1
+'use strict';
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to recursively match all subfolders:
+// 'test/spec/**/*.js'
+
+module.exports = function(grunt) {
+
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
+
+  // Define the configuration for all the tasks
+  grunt.initConfig({
+
+    // Project settings
+    express : {
+      options : {
+        port : process.env.PORT || 9000
+      },
+      dev : {
+        options : {
+          script : 'server.js',
+          debug : true
+        }
+      },
+      prod : {
+        options : {
+          script : 'server.js',
+          node_env : 'production'
+        }
+      }
+    },
+    open : {
+      server : {
+        url : 'http://localhost:<%= express.options.port %>'
+      }
+    },
+    watch : {
+      express : {
+        files : ['server.js', 'lib/{,*//*}*.{js,json}'],
+        tasks : ['express:dev'],
+        options : {
+          livereload : true,
+          nospawn : true
+          // Without this option specified express won't be reloaded
+        }
+      },
+      gruntfile : {
+        files : ['Gruntfile.js']
+      }
+    }
+  });
+
+  grunt.registerTask('serve', function(target) {
+    if (target === 'dist') {
+      return grunt.task.run(['build', 'express:prod', 'open', 'express-keepalive']);
+    }
+    grunt.task.run(['express:dev', 'open', 'watch']);
+  });
+
+  grunt.registerTask('default', ['serve']);
+};
---
+++ server.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// Module dependencies.
+var express = require('express');
+
+var app = express();
+var server = require('http').createServer(app);
+
+// Express Configuration
+app.configure('development', function() {
+  app.use(require('connect-livereload')());
+});
+
+app.configure(function () {
+  app.use(express.logger('dev'));
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.cookieSession({
+    secret: 'ffoisaiods984'
+  }));
+  app.use(express.methodOverride());
+  // prevent IE caching
+  app.use(function (req, res, next) {
+    res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.header("Pragma", "no-cache");
+    res.header("Expires", 0);
+    res.send('hello ' + req.get('remote_user'));
+  });
+});
+
+// bootstrap to get sites and users array async when using mongoose
+// Start server
+var port = process.env.PORT || 3000;
+server.listen(port, function () {
+  console.log('Express server listening on port %d in %s mode', port, app.get('env'));
+});
